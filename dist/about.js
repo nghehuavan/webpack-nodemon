b1e743734a132eb385e7df800d3e0cf32c8cac7a
nghe.huavan <nghe.huavan@gmail.com>
2022-07-18 01:42:43 +0700

    .

---
+++ dist/about.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************!*\
+  !*** ./client/about.js ***!
+  \*************************/
+console.log('about');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYWJvdXQuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2stbm9kZW1vbi1odG1sLWpzLy4vY2xpZW50L2Fib3V0LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdhYm91dCcpO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
